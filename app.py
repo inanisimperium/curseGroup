cc5f329fbf61b4f226ed86bccfde50128cfe8038
inanisimperium <122769153+inanisimperium@users.noreply.github.com>
2023-02-02 08:44:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('rabbit_model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -16,7 +16,7 @@ def predict():
     width = float(request.form['width'])
     prediction = model.predict([[height, width]])
     output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text=f'{output}')
+    return render_template('index.html', prediction_text=f'{output}')
 
 if __name__ == "__main__":
     app.run()
